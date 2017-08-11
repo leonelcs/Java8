d48f2c01d7818c2bbda2803eaa57c69244a88f35
Silva <llsa@gft.com>
2017-08-11 14:04:36 -0300

    new problems resolved

--- src/main/examples/AreFollowingPatterns.java
+++ src/main/examples/AreFollowingPatterns.java
@@ -1,5 +1,13 @@
 package main.examples;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
 public class AreFollowingPatterns {
 	
 	/**
@@ -41,15 +49,49 @@ Return true if strings follows patterns and false otherwise.
 		// TODO Auto-generated method stub
 		String[] strings1 = {"cat", "dog", "dog"};
 		String[] patterns1 = {"a", "b", "b"};
-		areFollowingPatterns(strings1, patterns1);
+		System.out.println(areFollowingPatterns(strings1, patterns1));
 
 		String[] strings2 = {"cat", "dog", "doggy"};
 		String[] patterns2 = {"a", "b", "b"};
-		areFollowingPatterns(strings1, patterns1);
+		System.out.println(areFollowingPatterns(strings2, patterns2));
 	}
 	
 	static boolean areFollowingPatterns(String[] strings, String[] patterns) {
+		Map<String,List<Integer>> map = new HashMap<>();
+		for (int i=0; i< patterns.length; i++) {
+			if (!map.containsKey(patterns[i])) {
+				List<Integer> list = new ArrayList<>();
+				list.add(i);
+				map.put(patterns[i], list);
+			} else {
+				List<Integer> list = map.get(patterns[i]);
+				list.add(i);
+				map.put(patterns[i], list);
+			}
+		}
 		
+		Set<String> words = new HashSet<>();
+		
+		List<Integer> resultado = new ArrayList<>();
+		map.entrySet().stream()
+			.forEach(m -> {
+				List<Integer> list = m.getValue();
+
+				String word = strings[list.get(0).intValue()];
+				if (!words.contains(word)) {
+					words.add(word);
+					Optional<Integer> value = list.stream()
+							.filter(l -> !strings[l.intValue()].equals(word) )
+							.findFirst();
+						if (value.isPresent()) {
+							resultado.add(value.get());
+						}
+				} else {
+					resultado.add(-1);
+				}
+		});
+		if (resultado.size() == 0)
+			return true;
 		return false;
 	}
 
